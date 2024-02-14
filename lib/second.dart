aca4b0346adad25856db7b974d28fef0e32962f7
lolopindik <dimaluka2014@gmail.com>
2024-02-15 00:54:19 +0300

    Split the main file

--- lib/second.dart
+++ lib/second.dart
@@ -1,48 +1,24 @@
 import 'package:flutter/material.dart';
-import 'second.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Registration',
-      theme: ThemeData(
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({Key? key}) : super(key: key);
+class AnotherScreen extends StatelessWidget {
+  const AnotherScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-        preferredSize: const Size.fromHeight(56.0),
-        child: AppBar(
-          leading: const Icon(Icons.account_box),
-          iconTheme: const IconThemeData(color: Colors.white), // Set back button color to white
-          flexibleSpace: Container(
-            decoration: const BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-                colors: [Colors.green, Colors.blue],
-              ),
+      appBar: AppBar(
+        iconTheme: const IconThemeData(color: Colors.white),
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.green, Colors.blue],
             ),
           ),
-          title: const Text('Registration', style: TextStyle(color: Colors.white),),
-          centerTitle: true,
         ),
+        title: const Text('Info', style: TextStyle(color: Colors.white),),
+        centerTitle: true,
       ),
       body: Container(
         decoration: const BoxDecoration(
@@ -73,7 +49,51 @@ class MyHomePage extends StatelessWidget {
                         alignment: Alignment.center,
                         child: TextField(
                           decoration: InputDecoration(
-                            hintText: "Email:",
+                            hintText: "Passport Series:",
+                            border: InputBorder.none,
+                            isCollapsed: true,
+                          ),
+                          maxLength: 10,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 40, right: 40, top: 20, bottom: 20),
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [Colors.white, Colors.grey],
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+                      child: const Align(
+                        alignment: Alignment.center,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Insurance:",
+                            border: InputBorder.none,
+                            isCollapsed: true,
+                          ),
+                          maxLength: 12,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 40, right: 40, top: 20, bottom: 20),
+                      decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [Colors.white, Colors.grey],
+                        ),
+                      ),
+                      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 5),
+                      child: const Align(
+                        alignment: Alignment.center,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            hintText: "Address:",
                             border: InputBorder.none,
                             isCollapsed: true,
                           ),
@@ -95,63 +115,11 @@ class MyHomePage extends StatelessWidget {
                         alignment: Alignment.center,
                         child: TextField(
                           decoration: InputDecoration(
-                            hintText: "Password:",
+                            hintText: "Military bidet number:s",
                             border: InputBorder.none,
                             isCollapsed: true,
                           ),
-                          maxLength: 16,
-                        ),
-                      ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(left: 40, right: 40, top: 20, bottom: 20),
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                          begin: Alignment.topLeft,
-                          end: Alignment.bottomRight,
-                          colors: [Colors.white, Colors.grey],
-                        ),
-                      ),
-                      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 5),
-                      child: const Align(
-                        alignment: Alignment.center,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: "Confirm password:",
-                            border: InputBorder.none,
-                            isCollapsed: true,
-                          ),
-                          maxLength: 16,
-                        ),
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Navigate to another screen here
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(builder: (context) => const AnotherScreen()),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.transparent, // Transparent background
-                        elevation: 0, // No shadow
-                        padding: const EdgeInsets.all(0), // No padding
-                      ),
-                      child: Container(
-                        decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
-                            colors: [Colors.green, Colors.blue],
-                          ),
-                        ),
-                        padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 40),
-                        child: const Text(
-                          'Continue',
-                          style: TextStyle(
-                            color: Colors.white,
-                          ),
+                          maxLength: 100,
                         ),
                       ),
                     ),
